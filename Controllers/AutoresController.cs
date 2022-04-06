530feb9f23ea5dbfa64f1cb6f15d78641c0a05b6
Jorge Calderon <calderon.rjorge@gmail.com>
2022-04-06 19:51:42 -0400

    Using Entity Framework Core, DbContext

---
+++ Controllers/AutoresController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAspNet.Entidades;
+
+namespace WebApiAspNet.Controllers
+{
+	[ApiController]
+	[Route("api/autores")]
+	public class AutoresController : ControllerBase
+	{
+		[HttpGet]
+		public ActionResult<List<Autor>> Get()
+		{
+			return new List<Autor>() {
+				new Autor () {Id = 1, Nombre = "Jorge"},
+				new Autor () {Id = 2, Nombre = "Fernando"}
+			};
+		}
+	}
+}
