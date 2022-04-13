cd0b62cadbd9b09c4416dd80f4cc238e898c360f
Jorge Calderon <calderon.rjorge@gmail.com>
2022-04-13 09:24:07 -0400

    Adding the second controller

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+
 
 namespace WebApiAspNet
 {
@@ -27,8 +29,8 @@ namespace WebApiAspNet
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-
-			services.AddControllers();
+			services.AddControllers().AddJsonOptions(x =>
+				x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve);
 
 			services.AddDbContext<ApplicationDbContext>(options =>
 				options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
