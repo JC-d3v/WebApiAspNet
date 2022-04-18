9a42797e99a5885fe6d759cb807099b44491d712
Jorge Calderon <calderon.rjorge@gmail.com>
2022-04-18 11:36:17 -0400

    Default Validations

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +10,18 @@ namespace WebApiAspNet.Entidades
 	public class Autor
 	{
 		public int Id { get; set; }
+		[Required(ErrorMessage = "El campo {0} es requerido")]
+		[StringLength(maximumLength: 5, ErrorMessage = "El campo {0} excede la cantidad limite de {1} caracteres")]
 		public string Nombre { get; set; }
+		[Range(18, 120)]
+		[NotMapped]
+		public int Edad { get; set; }
+		[CreditCard]
+		[NotMapped]
+		public string TarjetasDeCredito { get; set; }
+		[Url]
+		[NotMapped]
+		public string URL { get; set; }
 		public List<Libro> Libros { get; set; }
 	}
 }
