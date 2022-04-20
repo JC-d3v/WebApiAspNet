67e9aea908656b6b154c243fd176ede67acfd6c5
Jorge Calderon <calderon.rjorge@gmail.com>
2022-04-20 16:04:09 -0400

    Model validation

--- Entidades/Autor.cs
+++ Entidades/Autor.cs
@@ -8,22 +8,40 @@ using WebApiAspNet.Validaciones;
 
 namespace WebApiAspNet.Entidades
 {
-	public class Autor
+	public class Autor : IValidatableObject
 	{
 		public int Id { get; set; }
 		[Required(ErrorMessage = "El campo {0} es requerido")]
 		[StringLength(maximumLength: 5, ErrorMessage = "El campo {0} excede la cantidad limite de {1} caracteres")]
-		[PrimeraLetraMayuscula]
+		//[PrimeraLetraMayuscula]
 		public string Nombre { get; set; }
-		[Range(18, 120)]
-		[NotMapped]
-		public int Edad { get; set; }
-		[CreditCard]
-		[NotMapped]
-		public string TarjetasDeCredito { get; set; }
-		[Url]
-		[NotMapped]
-		public string URL { get; set; }
+		//[Range(18, 120)]
+		//[NotMapped]
+		//public int Edad { get; set; }
+		//[CreditCard]
+		//[NotMapped]
+		//public string TarjetasDeCredito { get; set; }
+		//[Url]
+		//[NotMapped]
+		//public string URL { get; set; }
+		public int Menor { get; set; }
+		public int Mayor { get; set; }
 		public List<Libro> Libros { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (!string.IsNullOrEmpty(Nombre))
+			{
+				var primeraLetra = Nombre[0].ToString();
+				if (primeraLetra != primeraLetra.ToUpper())
+				{
+					yield return new ValidationResult("La primera letra debe ser mayusculas", new string[] { nameof(Nombre) });
+				}
+			}
+			if (Menor > Mayor)
+			{
+				yield return new ValidationResult("Este valor no puede ser mas grande que el campo Mayor", new string[] { nameof(Menor) });
+			}
+		}
 	}
 }
